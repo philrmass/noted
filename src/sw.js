3204f51f78318a1458d092a4967557913cb95800
Phil Mass <philrmass@gmail.com>
2022-09-09 14:54:40 -0700

    remove slash

--- src/sw.js
+++ src/sw.js
@@ -3,7 +3,7 @@ import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
 setupRouting();
 
 const files = getFiles();
-const local = files.map((file) => ({ ...file, url: `/noted/${file.url}` }));
+const local = files.map((file) => ({ ...file, url: `/noted${file.url}` }));
 console.log('files', files);
 console.log('local', local);
 setupPrecaching(local);

ee6c5b33f079e03a4bcc8e775f7b50a63b22b18f
Phil Mass <philrmass@gmail.com>
2022-09-09 15:19:18 -0700

    add start_url

--- src/sw.js
+++ src/sw.js
@@ -3,9 +3,9 @@ import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
 setupRouting();
 
 const files = getFiles();
-const pathed = files.map((file) => ({ ...file, url: `noted/${file.url}` }));
+//const pathed = files.map((file) => ({ ...file, url: `noted/${file.url}` }));
 console.log('files', files);
-console.log('pathed', pathed);
+//console.log('pathed', pathed);
 
-const used = pathed;
+const used = files;
 setupPrecaching(used);

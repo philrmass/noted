d425da4684f29a0bd8d915e79a70b859ab22c73b
Phil Mass <philrmass@gmail.com>
2022-09-09 14:15:45 -0700

    make files with subpath

--- src/sw.js
+++ src/sw.js
@@ -3,5 +3,5 @@ import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
 setupRouting();
 
 const files = getFiles();
-const local = files.map((file) => ({ ...file, url: `.${file.url}` }));
+const local = files.map((file) => ({ ...file, url: `/noted/${file.url}` }));
 setupPrecaching(local);

c44c4e7dfdf3d8e88e8dee55467e36b6029c09b8
Phil Mass <philrmass@gmail.com>
2022-09-09 14:47:03 -0700

    remove scope

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,8 @@
 export default (config, env, helpers) => {
   const isDev = process.env.NODE_ENV === 'development';
   //config.output.publicPath = isDev ? '' : '/noted/';
-  config.output.publicPath = '/noted/';
+  // config.output.publicPath = '/noted/';
+  config.output.publicPath = '/';
   //console.log("CONFIG", isDev);
 
   // use the public path in your app as 'process.env.PUBLIC_PATH'

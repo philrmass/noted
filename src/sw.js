dea21742d30b0277445b80dc68861325628531d4
Phil Mass <philrmass@gmail.com>
2022-09-09 15:05:50 -0700

    remove noted

--- src/sw.js
+++ src/sw.js
@@ -3,7 +3,9 @@ import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
 setupRouting();
 
 const files = getFiles();
-const local = files.map((file) => ({ ...file, url: `/noted${file.url}` }));
+const local = files.map((file) => ({ ...file, url: `/noted/${file.url}` }));
 console.log('files', files);
 console.log('local', local);
-setupPrecaching(local);
+
+const used = files;
+setupPrecaching(used);

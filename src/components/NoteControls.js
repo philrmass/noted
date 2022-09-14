38b9880b1b8e1929581dc0577c29a08432ed5860
Phil Mass <philrmass@gmail.com>
2022-09-13 22:47:46 -0700

    add components

--- src/components/NoteControls.js
+++ src/components/NoteControls.js
@@ -1,54 +1,53 @@
-/*
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-
-import { removeNote } from '../redux/notesActions';
-
-import BigButton from './BigButton';
-import Dialog from './Dialog';
+//import { useState } from 'react';
+//import { useDispatch } from 'react-redux';
+//import { removeNote } from '../redux/notesActions';
+//import BigButton from './BigButton';
+//import Dialog from './Dialog';
 import Icon from './Icon';
-*/
 import styles from './NoteControls.module.css';
 
 export default function NoteControls({ parentId, id, childCount }) {
   /*
   const [isOpen, setIsOpen] = useState(false);
   const dis = useDispatch();
+  */
 
   const handleRemove = () => {
+    console.log('REMOVE');
+    /*
     if (!childCount) {
       dis(removeNote(parentId, id));
     } else {
       setIsOpen(true);
     }
+    */
   };
 
   const confirmRemove = () => {
+    console.log('confirm-REMOVE');
+    /*
     setIsOpen(false);
     dis(removeNote(parentId, id));
+    */
   };
 
   const buildChildren = () => {
     if (!childCount) {
-      return <div></div>;
+      return <div />;
     }
 
     return <div className={styles.count}>{childCount}</div>;
   };
-  */
 
   return (
     <>
       <div className={styles.controls}>
-        CTRL
-        {/*
         <button className={styles.button} onClick={handleRemove}>
           <Icon name='cross' />
         </button>
         <div className={styles.icon}>
           {buildChildren()} 
         </div>
-        */}
       </div>
       {/*
       <Dialog isOpen={isOpen}>
